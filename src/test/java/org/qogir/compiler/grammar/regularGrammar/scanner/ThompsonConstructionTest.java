cb896605f837f034d030ae7c7c47f16fb8f46221
hatimi <2457971966@qq.com>
2025-04-10 16:05:51 +0800

    1

--- src/test/java/org/qogir/compiler/grammar/regularGrammar/scanner/ThompsonConstructionTest.java
+++ src/test/java/org/qogir/compiler/grammar/regularGrammar/scanner/ThompsonConstructionTest.java
@@ -5,11 +5,16 @@ import org.qogir.compiler.grammar.regularGrammar.*;
 public class ThompsonConstructionTest {
     public static void main(String[] args) {
 
-        ParseRegex pr = new ParseRegex(new Regex("Any string including 'ab'","汉字汉字",0));
-
+        ParseRegex pr = new ParseRegex(new Regex("Correct", "c(a|b)*", 0));
+        //一个正确的输入
+        System.out.println(pr.parse().toString());
+        pr = new ParseRegex(new Regex("Error", "c(a|b))*", 0));
+        //一个错误的输入，存在一个右括号，没有左括号与之匹配
+        System.out.println(pr.parse().toString());
+        //RE2NFA转换
+        pr = new ParseRegex(new Regex("Correct", "c(a|b)*", 0));
         ThompsonConstruction tc = new ThompsonConstruction();
         TNFA tN = tc.translate(pr.parse().getRoot());
         System.out.println(tN.toString());
-        System.out.println(Character.isLetter('你'));
-    }
+}
 }
